0fd6359fb9a6b667b10c08703eb3efa3f52baf7e
YuliyaKVAS <yuliya2095@bk.ru>
2018-11-15 00:24:29 +0300

    MyProfile is changed

--- my-app/src/Components/MyProfile.js
+++ my-app/src/Components/MyProfile.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import ReserveForm from './ReserveForm';
 import Typography from '@material-ui/core/Typography';
-
+import Paper from '@material-ui/core/Paper';
 
 
 const styles = theme => ({
@@ -27,6 +27,10 @@ const styles = theme => ({
     marginRight: '48px',
     margin: 'auto',
     color: "#F9FBE7"
+  },
+  paper:{
+    margin:'24px',
+    padding: '24px'
   }
 });
 
@@ -35,13 +39,34 @@ const {classes} = props;
   return(
     <div className={classes.root}>
       <Grid container spasing={24} style={{background:'rgba(0,0,255,0.1)'}}>
-        <Grid item sm={12} className={classes.item}>
-          <Typography paragraph component='p' variant="h6" className={classes.text}>
-            Dear customer, you are on your profile page! Here you can make an appointment for a convenient day.
-          </Typography>
+        <Grid item sm={6}>
+          <Paper className={classes.paper}>
+            <Typography variant="h5" component="h3">
+              The main concept of our salon is quality
+            </Typography>
+            <Typography component="p">
+              All our masters are professionals
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item sm={6}>
+          <Paper className={classes.paper}>
+            <Typography variant="h5" component="h3">
+              The main concept of our salon is quality
+            </Typography>
+            <Typography component="p">
+              All our masters are professionals
+            </Typography>
+          </Paper>
         </Grid>
         <Grid item sm={12} className={classes.item}>
-        <ReserveForm />
+          <ReserveForm />
+        </Grid>
+        <Grid item sm={12}>
+          <Typography component="p" variant="h6">
+            If you still have any questions, please write us in our profiles in social networks or call us <br/>
+            +44-7871234567
+          </Typography>
         </Grid>
       </Grid>
     </div>
